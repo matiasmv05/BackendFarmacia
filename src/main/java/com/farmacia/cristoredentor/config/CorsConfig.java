c613319c78e8c7b6d9eefe7d6d18c5b958db56c3
matias <matiasmv2005@gmail.com>
2026-06-16 08:49:47 -0400

    ultimo cambio al back- cors

--- src/main/java/com/farmacia/cristoredentor/config/CorsConfig.java
+++ src/main/java/com/farmacia/cristoredentor/config/CorsConfig.java
@@ -1,7 +1,9 @@
 package com.farmacia.cristoredentor.config;
 
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,10 +13,15 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
+    @Value("${CORS_ORIGINS:http://localhost:3000}")
+      private String allowedOriginsRaw;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+        List<String> origins = Arrays.asList(allowedOriginsRaw.split(","));
+
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("*")); // permite todo temporalmente
+        config.setAllowedOrigins(origins);
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
